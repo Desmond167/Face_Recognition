834f60082dd1641fa5094e472e7a360b6b11e4ff
Soham <sohamchakraborty777@gmail.com>
2021-06-15 23:42:36 +0530

    test

--- FACE_DETECTION/01_run_face_detector.py
+++ FACE_DETECTION/01_run_face_detector.py
@@ -1,6 +1,5 @@
 from detect_face import extract_face
-from FACE_DETECTION.detect_face import extract_face
-from FACE_DETECTION.detector_config import DetectorConfig
+from detector_config import DetectorConfig
 
 
 CONFIG = DetectorConfig()
