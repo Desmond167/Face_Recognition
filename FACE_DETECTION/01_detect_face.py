8871b532df592ebd5c10c525d89cbc69d40a1f63
Soham <sohamchakraborty777@gmail.com>
2021-06-15 22:46:49 +0530

    test

---
+++ FACE_DETECTION/01_detect_face.py
@@ -0,0 +1,21 @@
+from PIL import Image as im
+
+################ Extract a face from a given photograph ################
+def extract_face(filename, required_size=(224, 224)):
+	# load image from file
+	pixels = im.open("sharon.jpg")
+	# create the detector, using default weights
+	detector = MTCNN()
+	# detect faces in the image
+	results = detector.detect_faces(pixels)
+	# extract the bounding box from the first face
+	x1, y1, width, height = results[0]['box']
+	x2, y2 = x1 + width, y1 + height
+	# extract the face
+	face = pixels[y1:y2, x1:x2]
+	# resize pixels to the model size
+	image = im.fromarray(face)
+	image = image.resize((160,160))
+	face_array = asarray(image)
+    PIL_image = im.fromarray(np.uint8(numpy_image)).convert('RGB')
+    print(PIL_image)
