33edf44d68ecba4c3681cf3e3dc9a023e6d57dfa
Soham <sohamchakraborty777@gmail.com>
2021-06-16 00:19:23 +0530

    test

--- FACE_DETECTION/detector_config.py
+++ FACE_DETECTION/detector_config.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from collections import namedtuple
 
 
 OUTPUT_IMAGE_SIZE = (160,160)
@@ -8,10 +9,12 @@ DEVICE = "cuda:0"
 class DetectorConfig:
 
     def configuration(self):
-        settings = {
+        settings_dict = {
             "INPUT_IMAGE" : "../sharon.jpg",
             "OUTPUT_IMAGE_SIZE" : OUTPUT_IMAGE_SIZE,
             "DEVICE" : DEVICE
         }
 
-        return settings
+        SETTINGS = namedtuple("SETTINGS", settings_dict.keys())(*settings_dict.values())
+
+        return SETTINGS
